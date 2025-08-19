30971057a4f61bc85461ab9cf6f021160212a7e8
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-08-19 18:59:12 -0300

    Parametro-rest

---
+++ 07-Parametro-rest/index.js
@@ -0,0 +1,4 @@
+function incentivarQuester(mensagem, ...nomesQuesters){
+    nomesQuesters.map(nomeQuester => console.log(`${mensagem} ${nomeQuester}`))
+}
+incentivarQuester(`Parabéns por ter chego ao módulo de JavaScript avançado,`, 'João', 'Pedro', 'Raphael')
