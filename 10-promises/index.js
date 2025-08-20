8a2b5176e96363756294e3e9a9c340472a31ce80
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-08-19 21:09:08 -0300

    Promises

---
+++ 10-promises/index.js
@@ -0,0 +1,27 @@
+/*
+    Pendente
+    Realizada Resolve
+    Recusada Reject
+    Estabelecida
+*/ 
+
+// new Promise((resolve, reject) => {
+
+// })
+
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaLigado) => {
+    return new Promise((resolve, reject) =>{
+        if (chaleiraEstaNoFogao && fogaoEstaLigado){
+            console.log('começando o processo de ferver água');
+            resolve()
+        }else{
+            console.log('É necessário ligar o fogão e colocar a chaleira no fogão para ferver a água')
+            reject()
+        }
+    })
+}
+
+let chaleiraEstaNoFogao = true
+let fogaoEstaLigado = true
+ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado)
+console.log('fazendo café');
