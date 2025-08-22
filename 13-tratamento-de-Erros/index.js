2b5f24e681d3431518a69dbf2dc5da34b152ccd1
Guilhermebsdourado <guilhermebarsilva@gmail.com>
2025-08-22 11:23:30 -0300

    tratamento-de-Erros

---
+++ 13-tratamento-de-Erros/index.js
@@ -0,0 +1,55 @@
+let ferverAgua = (chaleiraEstaNoFogao, fogaoEstaLigado) => {
+    return new Promise((resolve, reject) => {
+
+        if(typeof chaleiraEstaNoFogao != 'boolean') throw "Somente o tipo booleano é aceito"
+
+        if (chaleiraEstaNoFogao && fogaoEstaLigado) {
+            console.log('Passo 1 finalizado: Água foi fervida')
+            resolve()
+        } else {
+            console.log('é necessário colocar a chaleira com a água e ligar o fogão senão teu cafézinho não vai ficar pronto nunca')
+            reject()
+        }
+    })
+}
+
+let passarOCafe = (aguaFervida) => {
+    return new Promise((resolve) => {
+        console.log('Passo 2 finalizando: Café foi passado');
+        resolve(true)
+    })
+}
+
+let tomarCafe = (cafePassado) => {
+    return new Promise((resolve) => {
+        console.log('Passo 3 finalizado: Terminei de tomar o café')
+        resolve(true)
+    })
+}
+
+let lavarXicara = (cafeTomado) => {
+    return new Promise((resolve) => {
+        console.log('Passo 4 finalizado: Terminei de lavar a Xicara')
+        resolve(true)
+    })
+}
+
+let chaleiraEstaNoFogao = 'teste'
+let fogaoEstaLigado = true
+
+async function iniciarProcessoDeFazerCafe() {
+    try{
+        const aguaFervida = await ferverAgua(chaleiraEstaNoFogao, fogaoEstaLigado)
+        const cafePassado = await passarOCafe(aguaFervida)
+        const cafeTomado = await tomarCafe(cafePassado)
+        const xicaraLavada = await lavarXicara(cafeTomado)
+
+        // twrow "Mwnsagem de erro"
+    }catch(erro){
+        console.log(erro) 
+    }finally{
+        console.log('Finalizado o ritual de tomar o café, bora trabalhar')
+    }
+}
+
+iniciarProcessoDeFazerCafe()
